9ffaa3c228268428fa1b06dfe56ea9708a227275
IlyaStepanov1104 <ilyahtml@gmai.com>
2024-02-16 17:54:13 +0300

    Do scripts and styles

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-u«Z
+let cross = document.querySelector(".cross")
+
+document.querySelectorAll('.js-scale-img').forEach(function(el){
+    el.addEventListener('click', function(){
+        el.classList.add('active')
+        cross.classList.add('active')
+        cross.addEventListener('click', function(){
+            el.classList.remove('active')
+            cross.classList.remove('active')
+        })
+    })
+})
