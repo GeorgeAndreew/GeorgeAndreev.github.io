528fb323dda18d1c133dc275f83b5763218a42d1
GeorgeAndreew <georgy2008georgy@gmail.com>
2024-02-19 20:32:15 +0300

    Mobile adaptation p1

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ document.querySelectorAll('.js-scale-img').forEach(function(el){
         })
     })
 })
+let ava = document.querySelector('.AboutMeImg')
+document.addEventListener('mousemove', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 20 * dx / window.innerWidth / 2
+    let angleY = 20 * dy / window.innerHeight / 2
+    block.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`
+})
